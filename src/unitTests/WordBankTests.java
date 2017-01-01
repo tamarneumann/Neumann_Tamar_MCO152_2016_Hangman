63e1b49d224dfaa61ee0edffdc1cdab050dbf0d5
tamar neumann <tamar neumann>
2017-01-01 02:11:15 -0500

    Added unit tests take2

--- src/unitTests/WordBankTests.java
+++ src/unitTests/WordBankTests.java
@@ -1,5 +1,41 @@
 package unitTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import wordBank.WordBank;
+
 public class WordBankTests {
+	
+	private WordBank wb;
+	
+	@Before
+	public void createWordBank()
+	{
+		 wb=new WordBank();
+	}
+	
+	@Test
+	public void testLettersLeftCorrectAmount()
+	{
+	    
+		assertEquals(26, wb.getLettersLeft().size());	
+		wb.removeGuessedLetter('A');
+		assertEquals(25, wb.getLettersLeft().size());
+	}
+	
+	@Test
+	public void testLettersGuessedBeginsEmpty()
+	{
+		assertEquals(0,wb.getLettersGuessed().size());
+	}
+	
+	@Test
+	public void testLettersGuessedCorrectAmount()
+	{
+		wb.removeGuessedLetter('A');
+		assertEquals(1, wb.getLettersGuessed().size());
+	}
 
 }
